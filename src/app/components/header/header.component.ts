aeaff5ee99cb8ada796dd5323e41c982a47ccbd1
Hoang Phan <hoangphan29091999@gmail.com>
2024-09-08 14:44:18 +0700

    fixed collapse

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostListener } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 @Component({
   selector: 'app-header',
@@ -7,11 +7,34 @@ import { CartService } from '../../services/cart.service';
 })
 export class HeaderComponent implements OnInit {
   cartItemCount: number = 0; // Biến để lưu số lượng sản phẩm trong giỏ hàng
-
+  isMobile: boolean = false;
+  menuOpen: boolean = false;
   constructor(private cartService: CartService) {}
   ngOnInit(): void {
     this.cartService.cartItems$.subscribe(items => {
       this.cartItemCount = this.cartService.getTotalItemCount();
     });
+    this.checkScreenSize();
+  }
+   // Lắng nghe thay đổi kích thước màn hình
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.checkScreenSize();
+  }
+
+  // Kiểm tra kích thước màn hình chỉ khi có `window`
+  checkScreenSize(): void {
+    if (this.isBrowser()) {
+      this.isMobile = window.innerWidth < 768; // Dưới 768px là mobile
+    }
+  }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  // Kiểm tra nếu đang chạy trên browser
+  isBrowser(): boolean {
+    return typeof window !== 'undefined';
   }
 }
