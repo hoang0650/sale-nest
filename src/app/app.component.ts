72d8ea98697b085034f47bbd7784ed9bd8fde9f0
hoang0650 <48677925+hoang0650@users.noreply.github.com>
2024-09-21 17:36:46 +0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sale';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    // Vô hiệu hóa chuột phải
+    document.addEventListener('contextmenu', function (e) {
+      e.preventDefault();
+    });
+  }
+
+  // Vô hiệu hóa các phím F12 và Ctrl+Shift+I
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I')) {
+      event.preventDefault();
+    }
+  }
 }
+
